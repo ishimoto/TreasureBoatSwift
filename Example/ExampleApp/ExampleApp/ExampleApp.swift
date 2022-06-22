29da1ce30e5781a27fb91dedc31f7c2b722adf74
Ken Ishimoto <k.ishimoto@treasureboat.org>
2022-06-22 13:22:36 +0900

    add Example App

---
+++ Example/ExampleApp/ExampleApp/ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleApp.swift
+//  ExampleApp
+//
+//  Created by Ken Ishimoto on 2022/06/22.
+//
+
+import SwiftUI
+
+@main
+struct ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
