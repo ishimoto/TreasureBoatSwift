c5727814deed88240bd4941ad8f681aa8e4873af
Ken Ishimoto <k.ishimoto@treasureboat.org>
2022-06-22 13:35:47 +0900

    add platforms (test)

--- Package.swift
+++ Package.swift
@@ -5,6 +5,12 @@ import PackageDescription
 
 let package = Package(
     name: "TreasureBoatSwift",
+    platforms: [
+        .iOS(.v15),
+        .macOS(.v10_15),
+        .tvOS(.v9),
+        .watchOS(.v2)
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
