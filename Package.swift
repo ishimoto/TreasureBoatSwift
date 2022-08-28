dc41eeb07f067bbf69f0cc641d9d457f4ab8289f
Ken Ishimoto <k.ishimoto@treasureboat.org>
2022-08-29 00:08:52 +0900

    update Package

--- Package.swift
+++ Package.swift
@@ -3,11 +3,13 @@
 
 import PackageDescription
 
+// https://developer.apple.com/documentation/packagedescription
+
 let package = Package(
     name: "TreasureBoatSwift",
     platforms: [
         .iOS(.v15),
-        .macOS(.v10_15),
+        .macOS(.v11),
         .tvOS(.v9),
         .watchOS(.v8)
     ],
@@ -15,28 +17,52 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "TreasureBoatSwift",
-            targets: ["TreasureBoatSwift"]),
+            targets: [
+                "TreasureBoatSwift", "TBSwiftiOS"
+            ]
+        )
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         
         /* SFSafe Symbols */
-        .package(url: "https://github.com/SFSafeSymbols/SFSafeSymbols", .upToNextMajor(from: "3.0.0"))
+        .package(url: "https://github.com/SFSafeSymbols/SFSafeSymbols", from: Version(3, 0, 0))
+        
+        /* Thanks to CheckDevice 1.0.5, becaus it does not work on macOS the source code moved in and got fixed */
+        // .package(url: "https://github.com/ugurethemaydin/CheckDevice", from: Version(1, 0, 0))
+
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
+            // The name of the target.
             name: "TreasureBoatSwift",
+            // The target’s dependencies on other entities inside or outside the package.
             dependencies: [
                 "SFSafeSymbols"
-            ]),
-        .testTarget(
-            name: "TreasureBoatSwiftTests",
+            ],
+            // The path of the target, relative to the package root.
+            path: "Sources",
+            // The paths to source and resource files that you don’t want to include in the target.
+            exclude: [
+            ],
+            // The explicit list of resource files in the target.
+            resources: [
+                .process("Resources/Media.xcassets"),
+                .process("Images")
+            ]
+        ),
+        .target(
+            name: "TBSwiftiOS",
             dependencies: [
                 "TreasureBoatSwift",
                 "SFSafeSymbols"
-            ]),
+            ],
+            path: "Souces_iOS",
+            resources: []
+        )
+
     ]
 )
--- README.md
+++ README.md
@@ -7,8 +7,8 @@ a Swift Framework with TB Touch.
 ![SwiftUI](https://img.shields.io/badge/SwiftUI-3-blue)
 ![iOS](https://img.shields.io/badge/iOS-15%2B-orange)
 ![macOS](https://img.shields.io/badge/macOS-12%2B-orange)
-![tvOS](https://img.shields.io/badge/tvOS-2%2B-orange)
-![watch OS](https://img.shields.io/badge/watchOS-2%2B-orange)
+![tvOS](https://img.shields.io/badge/tvOS-9%2B-orange)
+![watch OS](https://img.shields.io/badge/watchOS-8%2B-orange)
 
 ### Creating a Swift Package
 
@@ -20,7 +20,39 @@ Following Information can be very helpful
 
 * [SFSafeSymbols](https://github.com/SFSafeSymbols/SFSafeSymbols)
 
-        
+
+### 2022/08/22
+
+* Add TBCheckDevice (originally CheckDevice from https://github.com/ugurethemaydin/CheckDevice) moved and fixed so it works on macOS
+
+* Add Color+ Extension
+
+```swift
+    Color(hex: "AFC") // RGB (12-bit)
+    Color(hex: "AAFFCC") // RGB (24-bit)
+    Color(hex: "33AAFFCC") // ARGB (32-bit)
+    
+    Color.orange.rgbAsHex -> String
+    Color.orange.rgbaAsHex -> String
+    Color.orange.argbAsHex -> String
+    Color.orange.isDark -> Bool
+```
+
+* Add UIWindow+ Extension
+
+```swift
+    public static func firstWindow() -> UIWindow?
+```
+
+* Add View+ Extension
+
+```swift
+
+    Text("some text")
+        .centered()
+
+```
+
 ### 2022/08/07
 
 Add 2 Views for TB Apps
@@ -38,3 +70,4 @@ Adding URL+ Extension
 Added a Shape
 
 * TBShapeRoundedCorners(corners: [.topLeft, .topRight], radius: 35)
+
