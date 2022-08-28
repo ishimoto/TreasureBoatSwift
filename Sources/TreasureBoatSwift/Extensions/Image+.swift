a4db5720cad4b3ddb3fe31655c72f62365eaa074
Ken Ishimoto <k.ishimoto@treasureboat.org>
2022-08-29 00:07:37 +0900

    add Images+ to read images from the SPM

---
+++ Sources/TreasureBoatSwift/Extensions/Image+.swift
@@ -0,0 +1,56 @@
+//
+//  Image+.swift
+//  
+//
+//  Created by Ken Ishimoto on 2022/08/21.
+//
+
+import SwiftUI
+import UIKit
+
+extension Image {
+    
+    /// Get the  Image form the Images Folder (TreasureBoatSwift Bundle).
+    /// - parameter assetName: the name of the image from this Bundle
+    /// - Returns: The Image.
+    public init(packageResource name: String, ofType type: String) {
+        
+#if canImport(UIKit)
+        guard let path = Bundle.module.path(forResource: name, ofType: type),
+              let image = UIImage(contentsOfFile: path) else {
+            self.init(name)
+            return
+        }
+        self.init(uiImage: image)
+        
+#elseif canImport(AppKit)
+        guard let path = Bundle.module.path(forResource: name, ofType: type),
+              let image = NSImage(contentsOfFile: path) else {
+            self.init(name)
+            return
+        }
+        self.init(nsImage: image)
+        
+#else
+        self.init(name)
+        
+#endif
+    }
+    
+#if canImport(UIKit)
+
+    /// Get the  Image form the Media Asset Catalog (TreasureBoatSwift Bundle).
+    /// - parameter assetName: the name of the asset from this Bundle
+    /// - Returns: The Image.
+    public init(assetName: String) {
+        let uiImage = UIImage(named: assetName, in: .module, with: nil)
+        if uiImage == nil {
+            self.init(systemSymbol: .questionmark)
+            return
+        }
+        self.init(uiImage: uiImage!)
+    }
+    
+#endif
+
+}
