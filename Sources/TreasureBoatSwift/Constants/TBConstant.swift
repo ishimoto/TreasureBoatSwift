1583f6d3916a415ba272d2159ba679183c93f7c1
Ken Ishimoto <k.ishimoto@treasureboat.org>
2022-10-11 13:46:28 +0900

    add TBConstant

---
+++ Sources/TreasureBoatSwift/Constants/TBConstant.swift
@@ -0,0 +1,68 @@
+//
+//  TBConstant.swift
+//  
+//
+//  Created by Ken Ishimoto on 2022/10/10.
+//
+
+import SwiftUI
+
+public struct TBConstant {
+    
+    public struct Color {
+        
+        static let nameForBenzaiten: String = "ColorBenzaiten"
+        static let nameForBenzaitenLight: String = "ColorBenzaitenLight"
+        static let nameForBishamonten: String = "ColorBishamonten"
+        static let nameForBishamontenLight: String = "ColorBishamontenLight"
+        static let nameForBudai: String = "ColorBudai"
+        static let nameForBudaiLight: String = "ColorBudaiLight"
+        static let nameForDaikoku: String = "ColorDaikoku"
+        static let nameForDaikokuLight: String = "ColorDaikokuLight"
+        static let nameForEbisu: String = "ColorEbisu"
+        static let nameForEbisuLight: String = "ColorEbisuLight"
+        static let nameForFukurokuju: String = "ColorFukurokuju"
+        static let nameForFukurokujuLight: String = "ColorFukurokujuLight"
+        static let nameForJurōjin: String = "ColorJurōjin"
+        static let nameForFJurōjinLight: String = "ColorJurōjinLight"
+        
+    }
+    
+    public struct Image {
+        
+        public static let nameForBenzaiten: String = "Benzaiten.jpg"
+        public static let nameForBishamonten: String = "Bishamonten.jpg"
+        public static let nameForBudai: String = "Budai.jpg"
+        public static let nameForEbisu: String = "Ebisu.jpg"
+        public static let nameForDaikoku: String = "Daikoku.jpg"
+        public static let nameForFukurokuju: String = "Fukurokuju.jpg"
+        public static let nameForJurōjin: String = "Jurōjin.jpg"
+        
+    }
+    
+    public struct HeadImage {
+        
+        public static let nameForBenzaiten = "Benzaiten44mm"
+        public static let nameForBishamonten = "Bishamonten44mm"
+        public static let nameForBudai = "Budai44mm"
+        public static let nameForDaikoku = "Daikoku44mm"
+        public static let nameForEbisu = "Ebisu44mm"
+        public static let nameForFukurokuju = "Fukurokuju44mm"
+        public static let nameForJurōjin = "Jurōjin44mm"
+        
+    }
+    
+    public struct Shadow {
+        
+        static let nameForBlackTransparentLight: String = "ColorBlackTransparentLight"
+        static let nameForBlacktransparentDark: String = "ColorBlacktransparentDark"
+
+    }
+
+    public struct AccentColor {
+        
+        static let nameForAccentColor: String = "AccentColor"
+        
+    }
+
+}
