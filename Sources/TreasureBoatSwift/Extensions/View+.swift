656ca42714ee954423034146c981f9fd9d25715a
Ken Ishimoto <k.ishimoto@treasureboat.org>
2022-10-11 13:47:06 +0900

    improve View

--- Sources/TreasureBoatSwift/Extensions/View+.swift
+++ Sources/TreasureBoatSwift/Extensions/View+.swift
@@ -17,4 +17,37 @@ extension View {
         }
     }
     
+    public func embedNavigationView() -> some View {
+        return NavigationView { self }
+    }
+    
+    public func screenRect() -> CGRect {
+#if os(iOS) || os(tvOS)
+        return UIScreen.main.bounds
+#elseif os(macOS)
+        return NSScreen.main!.visibleFrame
+#endif
+    }
+    
+    public func safeAreaBottom() -> CGFloat {
+#if os(iOS)
+        return UIWindow.firstWindow()?.safeAreaInsets.bottom ?? 10
+#else
+        return 10
+#endif
+    }
+    
+    func isMacOS()->Bool{
+        return TBCheckDevice.isMac()
+    }
+    
+#if os(iOS)
+    public func hideKeyboard() {
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder),
+                                        to: nil,
+                                        from: nil,
+                                        for: nil)
+    }
+#endif
+    
 }
