84ad742c3c3adbded17eed84c0f0ba1999024b52
Ken Ishimoto <k.ishimoto@treasureboat.org>
2022-10-30 20:31:45 +0900

    improve Observable

--- Sources/TreasureBoatSwift/S2M/TBObservableObject.swift
+++ Sources/TreasureBoatSwift/S2M/TBObservableObject.swift
@@ -5,8 +5,9 @@
 //  Created by Ken Ishimoto on 2022/10/18.
 //
 
-import Foundation
+import SwiftUI
 
+@MainActor
 open class TBObservableObject: ObservableObject {
     
     public init() {
@@ -14,11 +15,33 @@ open class TBObservableObject: ObservableObject {
     }
     
     @Published public var loadingState: TBLoadingState = .none
+
+    @Published public var showFailureMessage: Bool = false
     @Published public var failureMessage: String = ""
 
+    private func clearFailure() {
+        showFailureMessage = false
+        failureMessage = ""
+    }
+    
     public func clearMessage() {
-        self.failureMessage = ""
-        self.loadingState = .loading
+        clearFailure()
+        loadingState = .loading
+    }
+
+    @MainActor
+    public func showFailureMessage(_ message: String) {
+        let _message = String(describing: self) + " : \(message)"
+        
+        print(_message)
+        #warning("Fix LOG")
+        //log.info(_message) // TODO
+        
+        Task { @MainActor in
+            self.failureMessage = message
+            self.showFailureMessage = true
+            self.loadingState = .failed
+        }
     }
 
 }
