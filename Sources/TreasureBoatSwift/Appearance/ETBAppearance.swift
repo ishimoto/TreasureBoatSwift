565c87a176b67c2d4a46422ea9c5b69c6a6b6e9b
Ken Ishimoto <k.ishimoto@treasureboat.org>
2023-01-15 23:23:03 +0900

    add Appearance

---
+++ Sources/TreasureBoatSwift/Appearance/ETBAppearance.swift
@@ -0,0 +1,14 @@
+//
+//  ETBAppearance.swift
+//  
+//
+//  Created by Ken Ishimoto on 2023/01/15.
+//
+
+import Foundation
+
+public enum ETBAppearance: String {
+    case dark
+    case light
+    case system
+}
---
+++ Sources/TreasureBoatSwift/Appearance/TBAppearance.swift
@@ -0,0 +1,30 @@
+//
+//  TBAppearance.swift
+//  
+//
+//  Created by Ken Ishimoto on 2023/01/15.
+//
+
+import SwiftUI
+
+
+#if os(macOS)
+
+@available(OSX 11.0, *)
+struct TBAppearance {
+    
+    @AppStorage(TBConstant.AppStorage.AppTheme) var appTheme: ETBAppearance = .system
+    
+    func switchTheme() {
+        switch appTheme {
+        case .dark:
+            NSApp.appearance = NSAppearance(named: .darkAqua)
+        case .light:
+            NSApp.appearance = NSAppearance(named: .aqua)
+        default:
+            NSApp.appearance = nil
+        }
+    }
+}
+
+#endif
