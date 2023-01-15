36b5d92da7f73746cf28cce8aebcdc3b0a76c520
Ken Ishimoto <k.ishimoto@treasureboat.org>
2023-01-15 20:04:18 +0900

    add empty targets

---
+++ TBLogTarget/File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Ken Ishimoto on 2022/12/04.
+//
+
+import Foundation
---
+++ TBSwiftiOS/File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Ken Ishimoto on 2023/01/15.
+//
+
+import Foundation
