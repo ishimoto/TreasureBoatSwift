4773b91fa9c31332c771e15d932b41a82799d338
Ken Ishimoto <k.ishimoto@treasureboat.org>
2023-01-15 23:21:53 +0900

    fix QRCode Generatotion

--- Sources/TreasureBoatSwift/Image/TBQRCode.swift
+++ Sources/TreasureBoatSwift/Image/TBQRCode.swift
@@ -5,7 +5,7 @@
 //  Created by Ken Ishimoto on 2022/10/23.
 //
 
-#if os(iOS) || os(macOS)
+#if os(iOS)
 
 import SwiftUI
 import CoreImage
@@ -25,6 +25,33 @@ public struct TBQRCode {
             }
         }
         return UIImage(systemSymbol: .xmarkCircle)
+    }
+        
+}
+
+#endif
+
+
+#if os(macOS)
+
+import SwiftUI
+import CoreImage
+import CoreImage.CIFilterBuiltins
+
+public struct TBQRCode {
+    
+    public static func generateQRCode(from string: String, size: NSSize) -> NSImage {
+        let context = CIContext()
+        let filter = CIFilter.qrCodeGenerator()
+        
+        filter.message = Data(string.utf8)
+        
+        if let outputImage = filter.outputImage {
+            if let cgImage = context.createCGImage(outputImage, from: outputImage.extent) {
+                return NSImage(cgImage: cgImage, size: size)
+            }
+        }
+        return NSImage(systemSymbol: .xmarkCircle)
     }
         
 }
