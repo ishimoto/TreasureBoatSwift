d7655f85a491beeb11661f87738b5b29908c5c69
Ken Ishimoto <k.ishimoto@treasureboat.org>
2023-01-15 20:11:48 +0900

    remove old targets

--- Souces_iOS/TBSwiftiOS/File.swift
+++
@@ -1,8 +0,0 @@
-//
-//  File.swift
-//  
-//
-//  Created by Ken Ishimoto on 2022/08/21.
-//
-
-import Foundation
--- SourcesLogging/LoggerTarget/File.swift
+++
@@ -1,8 +0,0 @@
-//
-//  File.swift
-//  
-//
-//  Created by Ken Ishimoto on 2022/12/04.
-//
-
-import Foundation
