f8ef17ac4328a61eb4f89817aee9b23f27be494f
Ken Ishimoto <k.ishimoto@treasureboat.org>
2023-01-15 23:21:08 +0900

    fix Data+

--- Sources/TreasureBoatSwift/Extensions/Data+.swift
+++ Sources/TreasureBoatSwift/Extensions/Data+.swift
@@ -5,10 +5,12 @@
 //  Created by Ken Ishimoto on 2022/10/12.
 //
 
+#if (iOS || watchOS || tvOS)
+
 import UIKit
 
 public extension Data {
-
+    
     /// データ→イメージに変換する
     ///
     /// - Returns: 変換後のイメージ
@@ -19,5 +21,25 @@ public extension Data {
         }
         return image
     }
-
+    
 }
+
+#elseif (macOS)
+
+import AppKit
+
+public extension Data {
+    
+    /// データ→イメージに変換する
+    ///
+    /// - Returns: 変換後のイメージ
+    func toImage() -> NSImage {
+        guard let image = NSImage(data: self) else {
+            print("データをイメージに変換できませんでした。")
+            return NSImage()
+        }
+        return image
+    }
+    
+}
+#endif
